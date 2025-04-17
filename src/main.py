4892d526453aeb9af189dede2176447f86f12051
jman-berg <jogchumm@gmail.com>
2025-04-17 20:07:02 +0200

    textnode and texttypes

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+def main():
+    new_object = TextNode("some text", TextType.NORMAL, "GARDBADFDASFLKJA")
+    print(new_object)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+        def __init__(self, TEXT, TEXT_TYPE, URL):
+            self.text = TEXT
+            self.text_type = TEXT_TYPE
+            self.url = URL
+
+        def __eq__(self, other):
+            return self.text == other.text and self.text_type.value == other.text_type.value and self.url == other.url
+
+        def __repr__(self):
+            return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+            
+
