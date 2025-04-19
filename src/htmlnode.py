bedfc0ad6c9af807f828fdfa56e4a2ddd8b8d902
jman-berg <jogchumm@gmail.com>
2025-04-19 21:03:21 +0200

    added leafnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,20 +1,41 @@
-class HMTLNode:
+class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
 
-        def to_html(self):
-            raise NotImplementedError()
+    def to_html(self):
+        raise NotImplementedError()
 
-        def props_to_html(self):
-            html_string = ""
-            for k,v in self.props:
-                html_string += f' {k}="{v}"'
-            return html_string
+    def props_to_html(self):
+        html_string = ""
+        for k,v in self.props.items():
+            html_string += f' {k}="{v}"'
+        return html_string
 
-        def __repr__(self):
-            return f"tag: {self.tag} value: {self.value} children: {self.children} props: {self.props}"
+    def __repr__(self):
+        return f"tag: {self.tag} value: {self.value} children: {self.children} props: {self.props}"
+        
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None ):
+        super().__init__(tag, value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        
+        if self.tag is None:
+            return self.value
+
+        props_html = self.props_to_html() if self.props else ""
+        
+        return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
+
+
+
+
+
+
 
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,20 +1,31 @@
 import unittest
 
-from htmlnode import HMTLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        node = HMTLNode(tag="gurbe", value="girbe", children=["hoi", "doei"], props= {"href" : "https://www.google.nl"}) 
-        node2 = HMTLNode(tag="gurbe", value="girbe", children=["hoi", "doei"], props= {"href" : "https://www.google.nl"}) 
+        node = HTMLNode(tag="gurbe", value="girbe", children=["hoi", "doei"], props= {"href" : "https://www.google.nl"}) 
+        node2 = HTMLNode(tag="gurbe", value="girbe", children=["hoi", "doei"], props= {"href" : "https://www.google.nl"}) 
         self.assertNotEqual(node, node2)
 
     def test_tag(self):
-        node = HMTLNode(tag="gurbe", value="girbe", children=["hoi", "doei"], props= {"href" : "https://www.google.nl"}) 
-        self.assertIsNotNone(node.tag)
-
-    def test_props(self):
-        node = HMTLNode(tag="gurbe", value="girbe", children=["hoi", "doei"], props= {"href" : "https://www.google.nl"}) 
+        node = HTMLNode(tag="gurbe", value="girbe", children=["hoi", "doei"], props= {"href" : "https://www.google.nl"}) 
         self.assertIsNotNone(node.props)
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(),'<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "big and bold")
+        self.assertEqual(node.to_html(), "<b>big and bold</b>")
+
+
 
 if __name__ == "__main__":
     unittest.main()
