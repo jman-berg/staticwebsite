9e4d46e48d453ea4dddc22d7d48c3f51aaa66f62
jman-berg <jogchumm@gmail.com>
2025-04-29 17:22:08 +0200

    added recursive copy_static function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
-from textnode import TextNode, TextType
+from os.path import isfile
+import os, shutil
+
+def copy_static(source_dir=None, destination_dir=None):
+    if not source_dir or not destination_dir:
+        current_directory = os.getcwd()
+        destination_dir = f"{current_directory}/public"
+        source_dir = f"{current_directory}/static"
+        print(destination_dir)
+        #shutil.rmtree(destination_dir)
+        #os.mkdir(destination_dir)
+    else:
+        return
+    source_directories = os.listdir(source_dir)
+    for item in source_directories:
+        source_path = os.path.join(source_dir, item)
+        destination_path = os.path.join(destination_dir, item)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination_path)
+        else:
+            os.mkdir(destination_path)
+            #copy_static(source_path, destination_path)
 
 def main():
-    new_object = TextNode("some text", TextType.NORMAL, "GARDBADFDASFLKJA")
-    print(new_object)
+    copy_static()
 
 
 if __name__ == "__main__":
